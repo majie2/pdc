d1ed3186daa76c46ad95e38c42bd5434cb8a92c6
Josef Kelly <kelly.josef@gmail.com>
2010-12-28 17:38:29 -0800

    more comments...

--- fixinternet.sh
+++ fixinternet.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # no args
 # fixes the 70 rules file automagically to get internet config correctly
+# needs to be run as sudo (duh!)
 
 RULES="/etc/udev/rules.d/70-persistent-net.rules"
 
@@ -13,6 +14,8 @@ fi
 #does blank 70-persistant-net-rules exist?
 if [ -e blank/70-persistent-net.rules ] && [ -e $RULES ]; then
 	echo "finding module by interface"
+	#this will only find the first instance of INTERFACE, so hopefully, it is the right one
+	#although, if there are two instances, they should be the same module *crosses fingers*
 	interfaceLineNumber=`grep -n ${INTERFACE} ${RULES} | cut -f1 -d:`
 	let moduleLineNumber=$interfaceLineNumber-1
 	module=`sed -n "${moduleLineNumber}p" $RULES | cut -f2 -d'(' | cut -f1 -d')'`
