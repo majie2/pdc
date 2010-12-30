f417f719db32636664f7187383b7bd147bb8b0d1
Josef Kelly <kelly.josef@gmail.com>
2010-12-30 14:42:43 -0800

    finalized broker script

--- broker.sh
+++ broker.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
-# args 1: the location of the top folder containing invoices.pdf
-# args 2: the broker text file
+# args 1: the broker text file
+
+BILLINGDIR="/home/josef/Documents/sandbox"
 
 # invoice file
-INVOICE="${1}/invoices.pdf"
+INVOICE="${BILLINGDIR}/invoices.pdf"
 
-if [ ! -e ${2} ]; then
+if [ ! -e ${1} ]; then
 	exit 0
 fi
 
 # build invoice pdfs
 if [ -e ${INVOICE} ]; then
-	if [ ! -d ${1}/temp ]; then
-		mkdir ${1}/temp
+	if [ ! -d ${BILLINGDIR}/temp ]; then
+		mkdir ${BILLINGDIR}/temp
 	fi
 	
 	# get the number of pages in statement
@@ -23,32 +24,49 @@ if [ -e ${INVOICE} ]; then
 	rm ${INVOICE}.info.txt
 
 	# extract each page
-	#for (( j=1; j<=$invoicePages; j++ ))
-	#do
-	#	pdftotext -f $j -l $j ${INVOICE} ${1}/temp/page_$j.txt
-	#done
+	for (( j=1; j<=$invoicePages; j++ ))
+	do
+		pdftotext -f $j -l $j ${INVOICE} ${BILLINGDIR}/temp/page_$j.txt
+	done
 
 	while read line
 	do
-		path=`grep -lrw "$(echo -n $line | tr -d '\r')" ${1}/temp/`
+		path=`grep -lrw "$(echo -n $line | tr -d '\r')" ${BILLINGDIR}/temp/`
+
+		#echo $path
 
 		invoiceKey=`grep -n "Invoice Number:" $path | cut -f1 -d:`
 		let invoiceLineNumber=invoiceKey+1
 		invoice=`sed -n ${invoiceLineNumber}p $path`
 
 		amountKey=`grep -n "TOTAL INVOICE AMOUNT:" $path | cut -f1 -d:`
-		let lineNumber=amountKey+2
-		lineContents=`sed -n ${lineNumber}p $path`
 
-		if [ $lineContents = "$" ]; then
-			let amountLineNumber=amountKey+4
-			amount=`sed -n ${amountLineNumber}p $path`
-		else
-			amount="0.00"
+		let lineTwo=amountKey+2
+		let lineFour=amountKey+4
+		let lineSix=amountKey+6
+
+		lineContents=`sed -n ${lineTwo}p $path`
+
+		if [ "$lineContents" = "0.00" ]; then
+			amount=`sed -n ${lineTwo}p $path`
 		fi
 
-		echo "${invoice},${amount}"
-	done < ${2}
+		if [ "$lineContents" = "$" ]; then
+			amount=`sed -n ${lineFour}p $path`
+		fi
 
-	#rm -r ${1}/temp
+		if [ ! "$lineContents" = "0.00" ] && [ ! "$lineContents" = "$" ]; then
+			lineContents=`sed -n ${lineFour}p $path`
+			
+			if [ $lineContents = "$" ]; then
+				amount=`sed -n ${lineSix}p $path`
+			else
+				amount=`sed -n ${lineFour}p $path`
+			fi
+		fi
+
+		echo "$(echo -n $line | tr -d '\r'),${invoice},${amount}"
+	done < ${1}
+
+	rm -r ${BILLINGDIR}/temp
 fi
