4b5702667e35b1bec8554994b2a431b606bcc8bb
Josef Kelly <kelly.josef@gmail.com>
2011-03-09 12:34:28 -0800

    added statement splitting

--- qmbilling.sh
+++ qmbilling.sh
@@ -5,4 +5,20 @@
 #
 # description:
 # args 1:
-# args 2:
+
+# invoice file
+INVOICE="${1}/invoices.pdf"
+
+# check if the directory exists
+if [ -d ${1} ]; then
+	echo "found ${1}..."
+else
+	echo "could not find ${1}, exiting..."
+	exit 0
+fi
+
+# location where this script is being executed
+THIS_PATH="`dirname \"$0\"`"
+
+# split source pdf documents, uses the splitPDF script
+${THIS_PATH}/splitPDF.sh ${STATEMENT} ${1}/statements 2
